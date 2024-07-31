4b0b5ad4321abd117e3adf6cebee84d75bc9ebf3
Alexander Pyslarash <alex.pyslarash@gmail.com>
2024-07-31 13:08:41 -0700

    Table

--- tables.sql
+++ tables.sql
@@ -1,17 +1,16 @@
 CREATE TABLE users (
-    id int,
+    id int PRIMARY KEY,
     login varchar(255),
     email varchar(255),
     password varchar(255),
-    type varchar(255),
+    type varchar(255)
+);
 
-)
-
-CREATE TABLE users (
-    id int,
-    login varchar(255),
-    email varchar(255),
-    password varchar(255),
-    type varchar(255),
-
-)
+CREATE TABLE cities (
+    id int PRIMARY KEY,
+    user_id int,
+    city varchar(255),
+    country varchar(255),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    UNIQUE (city, country)
+);
