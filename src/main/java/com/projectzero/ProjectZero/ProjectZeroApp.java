aaaa3edb01e115d48326c36cb91bb0eed6bc0647
Alexander Pyslarash <alex.pyslarash@gmail.com>
2024-08-01 13:26:41 -0700

    Updated Tables

--- src/main/java/com/projectzero/ProjectZero/ProjectZeroApp.java
+++ src/main/java/com/projectzero/ProjectZero/ProjectZeroApp.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.projectzero")
-@EntityScan("com.revature.models")
-@EnableJpaRepositories("com.revature.repositories")
+@EntityScan("com.projectzero.models")
+@EnableJpaRepositories("com.projectzero.repositories")
 public class ProjectZeroApp {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectZeroApp.class, args);
+
 	}
 
 }
--- src/main/java/com/projectzero/services/UserService.java
+++ src/main/java/com/projectzero/services/UserService.java
@@ -4,6 +4,9 @@ import com.projectzero.models.User;
 import java.util.List;
 
 public interface UserService {
+
+
+
     User getUserById(Long id); // Change int to Long
     List<User> getAllUsers();
     User addUser(User user, String rawPassword);
