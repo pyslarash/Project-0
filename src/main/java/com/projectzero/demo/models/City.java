d990c8629ed7991b3fb678310bce74955720c7a0
MiguelArias01 <Argenis803@gmail.com>
2024-08-01 12:32:43 -0400

    added model

--- src/main/java/com/projectzero/demo/models/City.java
+++ src/main/java/com/projectzero/demo/models/City.java
@@ -1,18 +1,93 @@
 package com.projectzero.demo.models;
 
-
 import jakarta.persistence.*;
+
 import java.util.Objects;
 
-
 @Entity
-@Table(name = "cities")
-public Class City {
-    @Id //makes this a primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="a_id",updatable = false)
-    private int id;
-@Column(nullable = false,unique = true)
-private String name;
+@Table(name = "cities", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"city", "country"})
+})
+public class City {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "city", nullable = false, length = 255)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 255)
+    private String country;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Constructors
+    public City() {}
+
+    public City(User user, String city, String country) {
+        this.user = user;
+        this.city = city;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+        City city1 = (City) o;
+        return id == city1.id && Objects.equals(city, city1.city) && Objects.equals(country, city1.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city, country);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
