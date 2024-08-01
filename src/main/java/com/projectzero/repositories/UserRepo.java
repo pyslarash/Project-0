c37737ba060daab8899c41aa96368db4b42eb20f
Alexander Pyslarash <alex.pyslarash@gmail.com>
2024-08-01 14:36:32 -0700

    Set basic user mgmt - not working yet

--- src/main/java/com/projectzero/repositories/UserRepo.java
+++ src/main/java/com/projectzero/repositories/UserRepo.java
@@ -4,7 +4,11 @@ import com.projectzero.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> { // Change Integer to Long
-    User findByLogin(String login);
+
+    List<User> findUsersById(Long id);
+
 }
--- src/main/java/com/projectzero/services/UserService.java
+++ src/main/java/com/projectzero/services/UserService.java
@@ -1,15 +1,32 @@
 package com.projectzero.services;
 
 import com.projectzero.models.User;
+import com.projectzero.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
-public interface UserService {
+@Service
+public class UserService {
 
+    private final UserRepo userRepo;
 
+    @Autowired
+    public UserService(UserRepo userRepo){
+        this.userRepo = userRepo;
+    }
 
-    User getUserById(Long id); // Change int to Long
-    List<User> getAllUsers();
-    User addUser(User user, String rawPassword);
-    boolean authenticateUser(String login, String rawPassword);
-    boolean isAdmin(String login);
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepo.findById(id);
+    }
 }
